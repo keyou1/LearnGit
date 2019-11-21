1d04fffdc04ad16bddbfbb01358189e3eca41932
KeYou <keyou100@qq.com>
2019-11-21 14:38:55 +0800

    这是第一次提交的版本

--- test02.py
+++ test02.py
@@ -1,18 +1,19 @@
 import time
+
 # words = "Dear, I love you forever!"
 words = "I love you"
 for char in words.split():
-   allChar = []
-   for y in range(12, -12, -1):
-       lst = []
-       lst_con = ''
-       for x in range(-30, 30):
-            formula = ((x*0.05)**2+(y*0.1)**2-1)**3-(x*0.05)**2*(y*0.1)**3
+    allChar = []
+    for y in range(12, -12, -1):
+        lst = []
+        lst_con = ''
+        for x in range(-30, 30):
+            formula = ((x * 0.05) ** 2 + (y * 0.1) ** 2 - 1) ** 3 - (x * 0.05) ** 2 * (y * 0.1) ** 3
             if formula <= 0:
                 lst_con += char[(x) % len(char)]
             else:
                 lst_con += ' '
-       lst.append(lst_con)
-       allChar += lst
-   print('\n'.join(allChar))
-   time.sleep(1)
+        lst.append(lst_con)
+        allChar += lst
+    print('\n'.join(allChar))
+    time.sleep(1)
